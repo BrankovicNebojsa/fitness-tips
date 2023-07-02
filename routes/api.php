3449efb0dcb7d27aab857c7f0a7f501f2b335d26
Nebojsa Brankovic <n.brankovic99@gmail.com>
2023-07-02 23:02:57 +0200

    Zakomentarisan jedan api route

--- routes/api.php
+++ routes/api.php
@@ -25,9 +25,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/{id}', [UserController::class, 'show']);
-Route::post('/users', [UserController::class, 'store']);
 Route::put('/users/{id}', [UserController::class, 'update']);
 Route::delete('/users/{id}', [UserController::class, 'destroy']);
+// Route::post('/users', [UserController::class, 'store']); jer se ovo obavlja kroz login/register
 
 Route::get('/users/{id}/workouts', [UserWorkoutController::class, 'index'])->name('users.workouts.index');
 
